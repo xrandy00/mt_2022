b83ee26254894525915c1c50735ca7e2919d8d23
Votjěch Randýsek <xrandy00@fit.vutbr.cz>
2022-03-05 18:01:19 +0100

    missing files from last commit

--- js-vuln-det/src/evaluate.js
+++ js-vuln-det/src/evaluate.js
@@ -1,21 +1,89 @@
-console.log('in evaluate');
-const nodeList = document.querySelectorAll('script');
+console.log('in new evaluate');
 
-for (let i = 0; i < nodeList.length; ++i) {
-  const node = nodeList[i];
-  const ignore = node.getAttribute('ignore');
+function runScripts(element) {
+  var list, scripts, index;
 
-  if (ignore == 'true') {
-    continue;
+  // Get the scripts
+  list = element.getElementsByTagName("script");
+  scripts = [];
+  for (index = 0; index < list.length; ++index) {
+
+    const ignore = list[index].getAttribute('ignore');
+    if (!ignore) {
+      scripts[index] = list[index];
+    }
+
   }
+  list = undefined;
 
-  const script = node.innerHTML;
-  console.log()
-  if (script) {
-    try {
-      eval(script);
-    } catch (error) {
-      console.log('script could not have been evaluated', error);
+  // Run them in sequence
+  continueLoading();
+
+  function continueLoading() {
+    var script, newscript;
+
+    // While we have a script to load...
+    while (scripts.length) {
+      // Get it and remove it from the DOM
+      script = scripts[0];
+      script.parentNode.removeChild(script);
+      scripts.splice(0, 1);
+
+      // Create a replacement for it
+      newscript = document.createElement('script');
+
+      // External?
+      if (script.src) {
+        // Yes, we'll have to wait until it's loaded before continuing
+        newscript.onerror = continueLoadingOnError;
+        newscript.onload = continueLoadingOnLoad;
+        newscript.onreadystatechange = continueLoadingOnReady;
+        newscript.src = script.src;
+      } else {
+        // No, we can do it right away
+        newscript.text = script.text;
+      }
+
+      // Start the script
+      document.documentElement.appendChild(newscript);
+
+      // If it's external, wait
+      if (script.src) {
+        return;
+      }
+    }
+
+    // All scripts loaded
+    newscript = undefined;
+
+    // Callback on most browsers when a script is loaded
+
+    function continueLoadingOnLoad() {
+      // Defend against duplicate calls
+      if (this === newscript) {
+        continueLoading();
+      }
+    }
+
+    // Callback on most browsers when a script fails to load
+
+    function continueLoadingOnError() {
+      // Defend against duplicate calls
+      if (this === newscript) {
+        continueLoading();
+      }
+    }
+
+    // Callback on IE when a script's loading status changes
+
+    function continueLoadingOnReady() {
+
+      // Defend against duplicate calls and check whether the
+      // script is complete (complete = loaded or error)
+      if (this === newscript && this.readyState === "complete") {
+        continueLoading();
+      }
     }
   }
 }
+runScripts(document.documentElement);
---
+++ test_web/README.md
@@ -0,0 +1,5 @@
+Very simple website containing example of inline script and script referenced via URL.
+The purpose of this web is to test the js-vuln-det extension.
+
+Run for example with 
+python3 -m http.server
